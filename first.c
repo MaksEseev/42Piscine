2f4f98eda37d78ab860a3eeb945c2b2af3984be2
MaksEseev <111888185+MaksEseev@users.noreply.github.com>
2022-08-25 12:32:15 +0300

    Add files via upload

---
+++ first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void)
+{
+    int num;
+    num = 1;
+    printf("Яперсональный ");
+    printf("компьютер.\n");
+    printf("Мое любимое число %d, потому что оно первое.\n",num);
+getcgar();
+	return 0;
+}
